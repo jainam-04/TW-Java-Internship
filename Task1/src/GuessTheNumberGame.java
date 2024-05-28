4615187f323c199feada0d84da15ec9ae663634b
Jainam <jainamrupani04@gmail.com>
2024-05-28 20:09:25 +0530

    Task 1 Completed

--- Task1/src/GuessTheNumberGame.java
+++ Task1/src/GuessTheNumberGame.java
@@ -1,5 +1,28 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheNumberGame {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        Random random = new Random();
+        int randomNumber = random.nextInt(1, 100);
+        boolean flag = true;
+        int count = 0;
+        System.out.println("Welcome to the Guess The Number Game.\nGuess the number in the range from 1 to 100.");
+        while(flag){
+            System.out.print("Enter the number you have guessed :- ");
+            int guessedNumber = sc.nextInt();
+            count++;
+            if(guessedNumber < randomNumber){
+                System.out.println("You guessed the wrong number.\nGuess some bigger number.");
+            }
+            else if(guessedNumber > randomNumber){
+                System.out.println("You guessed the wrong number.\nGuess some smaller number.");
+            }
+            else{
+                System.out.println("Congrats!!\nYou guessed the correct number in " + count + " chances.");
+                flag = false;
+            }
+        }
     }
 }
