fd2626532a40435f831d007febc5f5c1e17f7e3e
Jainam <jainamrupani04@gmail.com>
2024-05-30 10:55:46 +0530

    Voter Login Page designing started

---
+++ Task3/src/VoterLogin.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VoterLogin extends JFrame implements ActionListener {
+    VoterLogin(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new VoterLogin();
+    }
+}
