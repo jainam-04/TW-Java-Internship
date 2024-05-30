a34df3c91f0ce96bcbb7e4531d600dafa7143627
Jainam <jainamrupani04@gmail.com>
2024-05-30 17:59:42 +0530

    Voter Registration Page 1 designing started

---
+++ Task3/src/VoterRegister1.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VoterRegister1 extends JFrame implements ActionListener {
+    VoterRegister1(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new VoterRegister1();
+    }
+}
