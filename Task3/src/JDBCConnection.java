70143e9d619d10b8f6b1f37cec06dc8f01c13615
Jainam <jainamrupani04@gmail.com>
2024-05-30 09:34:20 +0530

    JDBC Connection established

--- Task3/src/JDBCConnection.java
+++ Task3/src/JDBCConnection.java
@@ -1,5 +1,20 @@
-public class JDBCConnection {
-    JDBCConnection(){
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 
+public class JDBCConnection {
+    Connection connection;
+    Statement statement;
+    public JDBCConnection(){
+        String url = "jdbc:mysql://localhost:3306/voting_system";
+        String user = "root";
+        String password = "Rupani@0410";
+        try{
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
