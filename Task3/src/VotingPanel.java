fc364c4db39014951b696818310eeeef98cd1104
Jainam <jainamrupani04@gmail.com>
2024-05-31 20:16:06 +0530

    Voting Panel designing started

---
+++ Task3/src/VotingPanel.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class VotingPanel extends JFrame implements ActionListener {
+    VotingPanel(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new VotingPanel();
+    }
+}
